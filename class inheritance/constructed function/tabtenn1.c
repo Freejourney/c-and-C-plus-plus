a8165923b5fbc2132f7b4881c731cd13d78e030a
longlyjourney <983621131@qq.com>
2017-04-09 20:25:54 +0800

    Create tabtenn1.c

---
+++ class inheritance/constructed function/tabtenn1.c
@@ -0,0 +1,11 @@
+//tabtenn1.cpp -- simple base-class methods
+#include <iostream>
+#include "tabtenn1.h"
+
+TableTennisPlayer::TableTennisPlayer(const string & fn, const string & ln, const bool & ht):firstname(fn), lastname(ln), hasTable(ht){}
+
+void TableTennisPlayer::Name() const { std::cout << lastname << ", " << firstname << "\n"; }
+
+RatedPlayer::RatedPlayer(unsigned int r, const string & fn, const string & ln, const bool ht):rating(r), TableTennisPlayer(fn, ln, ht){}
+
+RatedPlayer::RatedPlayer(unsigned int r, const TableTennisPlayer & tp):TableTennisPlayer(tp), rating(r){}
